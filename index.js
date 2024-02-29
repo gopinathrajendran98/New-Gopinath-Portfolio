d453009c8430ad7c84ca5a870ca062dd32597ceb
gopinathrajendran98 <89033905+gopinathrajendran98@users.noreply.github.com>
2024-02-29 16:21:20 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Navbar from './components/Navbar/Navbar'
 
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
